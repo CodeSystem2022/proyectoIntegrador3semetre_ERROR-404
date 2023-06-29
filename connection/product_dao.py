59cfe01f4e55cc0ab81c102035af9a644a47b4ee
matias9486 <matias9486mdp@gmail.com>
2023-06-29 16:31:35 -0300

    feat/#5-clase-product_dao:Creación de la clase product_dao con las consultas y método de clase seleccionar

---
+++ connection/product_dao.py
@@ -0,0 +1,29 @@
+import sys
+from connection import Conexion
+from connection.product import Producto
+
+
+class ProductDAO:
+    #Creación de consultas y asignación a variables
+    _SELECCIONAR = 'SELECT * FROM producto ORDER BY id_producto'
+    _INSERTAR = 'INSERT INTO producto(nombre, precio) VALUES (%s, %s)'
+    _ACTUALIZAR = 'UPDATE producto SET nombre=%s, precio=%s WHERE id_producto=%s'
+    _ELIMINAR = 'DELETE FROM producto WHERE id_producto=%s'
+
+    #Definimos los métodos de clases requeridos
+    @classmethod
+    def seleccionar(cls):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                productos = []
+                try:
+                    cursor.execute(cls._SELECCIONAR)
+                    registros = cursor.fetchall()
+
+                    for registro in registros:
+                        producto1 = Producto(registro[0], registro[1], registro[2])
+                        productos.append(producto1)
+                except Exception as e:
+                    print(f"Ocurrio un Error: {e}")
+
+                return productos
