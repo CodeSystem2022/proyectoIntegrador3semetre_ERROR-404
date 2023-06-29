96a5c67a1374918e16ae5b671fca6f55005003f0
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-06-29 10:52:58 -0300

    agrego los getters de la clase producto

--- product.py
+++ product.py
@@ -1,5 +1,21 @@
 class Producto:
+    # constructor de la clase Producto
     def __init__(self, idproducto, nombre, precio):
         self._idproducto = idproducto
         self._nombre = nombre
         self._precio = precio
+
+    # getters de la clase producto
+    @property
+    def idproducto(self):
+        return self._idproducto
+
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @property
+    def precio(self):
+        return self._precio
+    
+    # TODO: faltan los setters y el metodo toString
